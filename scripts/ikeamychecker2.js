b395e00961b1e392b016aef8bfc59389c011b13a
weareblahs <tanyuxuan2005@gmail.com>
2022-07-18 14:03:27 +0800

    Fix blahaj bag part

--- scripts/ikeamychecker2.js
+++ scripts/ikeamychecker2.js
@@ -1,5 +1,5 @@
 function getstock2() {
-  // Go on, fork this repo then change the "var country" to your country code and "var productID" to
+  // Go on, fork this repo then change the "var country" to your country code and "var productID2" to
   // the product ID you're getting the stock for to get local IKEA product stock. You know, something
   // like "my" for Malaysia, "sg" for Singapore, etc. But you still need to change the location names
   //because IKEA API didn't have that. Do note that the "x-client-id" thing is "hardcoded" so you
@@ -10,10 +10,10 @@ function getstock2() {
   xhttp2.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("detailsPhysical-" + productID2).innerHTML = 
-      "IKEA Cheras: " + xhttp.response.data[0].availableStocks[0].quantity + 
-      "<br>IKEA Damansara: " + xhttp.response.data[1].availableStocks[0].quantity + 
-      "<br>IKEA Tebrau: " + xhttp.response.data[2].availableStocks[0].quantity +
-      "<br>IKEA Batu Kawan: " + xhttp.response.data[3].availableStocks[0].quantity;
+      "IKEA Cheras: " + xhttp2.response.data[0].availableStocks[0].quantity + 
+      "<br>IKEA Damansara: " + xhttp2.response.data[1].availableStocks[0].quantity + 
+      "<br>IKEA Tebrau: " + xhttp2.response.data[2].availableStocks[0].quantity +
+      "<br>IKEA Batu Kawan: " + xhttp2.response.data[3].availableStocks[0].quantity;
     }
   };
   xhttp2.open("GET", "https://api.ingka.ikea.com/cia/availabilities/ru/" + country + "?itemNos=" + productID2 + "&expand=StoresList,Restocks,SalesLocations", true);
